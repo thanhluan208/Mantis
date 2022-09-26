a2ea6166052a43eca819c904c5726494c458eb28
thanhluan208 <skthunte33@gmail.com>
2022-09-26 17:26:30 +0700

    update code

--- src/components/CommonStyles/index.js
+++ src/components/CommonStyles/index.js
@@ -2,12 +2,14 @@ import NumberFormatCustom from './NumberFormat';
 import TextField from './TextField';
 import Button from './Button';
 import Typography from './Typography';
+import Table from './Table';
 
 const CommonStyles = {
   TextField,
   NumberFormat: NumberFormatCustom,
   Button,
   Typography,
+  Table,
 };
 
 export default CommonStyles;
